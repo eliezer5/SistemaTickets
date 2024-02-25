5bcd8fc9f8a5c61e4b831aad9f4d3d69ac440ab0
eliezer terrero <eliezerterrero12@gmail.com>
2024-02-25 13:51:04 -0400

    Creando los Modelos Tickets y TicketsDetalles

---
+++ SistemaTicket/Models/Tickets.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTicket.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "La Fecha es requerida")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "El SolicitadoPor es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El SolicitadoPor no puede tener más de 100 caracteres.")]
+        public string? SolicitadoPor { get; set; }
+
+        [Required(ErrorMessage = "El Asunto es obligatorio.")]
+        [MaxLength(255, ErrorMessage = "El Asunto no puede tener más de 255 caracteres.")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "La Descripcion es obligatoria.")]
+        public string? Descripcion { get; set; }
+
+    }
+}
---
+++ SistemaTicket/Models/TicketsDetalles.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaTicket.Models
+{
+    public class TicketsDetalles
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [ForeignKey("TicketId")]
+        public int TicketId { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo no acepta digitos")]
+        public string Emisor {  get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo no acepta digitos")]
+        public string Mensaje { get; set; }
+    }
+}
