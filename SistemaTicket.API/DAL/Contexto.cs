1acfd9495eb5aafb39de9737d41c010cc44c305a
eliezer terrero <eliezerterrero12@gmail.com>
2024-02-25 13:51:42 -0400

    Creando el Contexto y la migracion en el server

---
+++ SistemaTicket.API/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaTicket.Models;
+
+namespace SistemaTicket.DAL
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        {
+
+        }
+
+        DbSet<Tickets> Tickets { get; set; }
+        DbSet<TicketsDetalles> TicketsDetalles { get; set; }
+
+    }
+}
---
+++ SistemaTicket.API/Migrations/20240225174838_initial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaTicket.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    SolicitadoPor = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Asunto = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketsDetalles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Emisor = table.Column<string>(type: "TEXT", nullable: false),
+                    Mensaje = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketsDetalles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "TicketsDetalles");
+        }
+    }
+}
